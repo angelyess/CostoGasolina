fc6b809600089b040551c883b0fda0d93cb167e3
Angel Yess <angruiz.yess@gmail.com>
2026-02-23 21:01:43 -0600

    Ejercicio 2 TSP

--- app/src/main/java/com/mexiti/costogasolina/MainActivity.kt
+++ app/src/main/java/com/mexiti/costogasolina/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.text.KeyboardOptions
@@ -15,13 +16,20 @@ import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import com.mexiti.costogasolina.ui.theme.CostoGasolinaTheme
+import java.text.NumberFormat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,7 +39,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = Color.LightGray
                 ) {
                     CostGasLayout("Android")
                 }
@@ -42,7 +50,25 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CostGasLayout(name: String) {
-    Column {
+    var precioLitroEntrada by remember{
+        mutableStateOf("")
+    }
+    var cantLitroEntrada by remember{
+        mutableStateOf("")
+    }
+    var propinaEntrada by remember{
+        mutableStateOf("")
+    }
+    val precioLitro = precioLitroEntrada.toDoubleOrNull() ?: 0.0
+    val cantLitro = cantLitroEntrada.toDoubleOrNull() ?: 0.0
+    val propina = propinaEntrada.toDoubleOrNull() ?: 0.0
+    val monto = calcularMonto(precioLitro, cantLitro, propina)
+
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Text(
             text = stringResource(R.string.calcular_monto),
 
@@ -54,23 +80,41 @@ fun CostGasLayout(name: String) {
                keyboardType = KeyboardType.Number,
                imeAction = ImeAction.Next
            ),
-           value = "10.0",
-           onValueChanged = {}
+           value = precioLitroEntrada,
+           onValueChanged = {
+               precioLitroEntrada = it
+           }
        )
-        TextField(
-            value = stringResource(R.string.litros),
-            onValueChange = {}
+        EditNumberField(
+            label = R.string.litros,
+            leadingIcon = R.drawable.gas_24,
+            keyboardsOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Done
+            ),
+            value = cantLitroEntrada,
+            onValueChanged = {
+                cantLitroEntrada = it
+            }
         )
-        TextField(
-            value = stringResource(R.string.propina) ,
-            onValueChange = {}
+        EditNumberField(
+            label = R.string.propina,
+            leadingIcon = R.drawable.propin_24,
+            keyboardsOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Done
+            ),
+            value = propinaEntrada,
+            onValueChanged = {
+                propinaEntrada = it
+            }
         )
         Switch(
-            checked = false,
-            onCheckedChange =  {}
+            checked = true,
+            onCheckedChange =  {propinaEntrada = ""}
         )
         Text(
-            text = "Total:  $"
+            text = stringResource(R.string.monto_total, monto   )
         )
 
     }
@@ -104,4 +148,8 @@ fun CostGasLayoutPreview() {
     CostoGasolinaTheme {
         CostGasLayout("Android")
     }
+}
+private fun calcularMonto(precio: Double, cantLitros: Double, cantPropina: Double):String{
+    val monto= precio * cantLitros+cantPropina
+    return NumberFormat.getCurrencyInstance().format(monto)
 }
